0679cc9f0e3a341edb57a55e13c6ebc254bc0891
Aderemi Oje <remoj2003@yahoo.com>
2024-12-26 22:48:24 +0100

    blackhost

---
+++ src/Home.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Skills from "./Skills";
+
+function Home() {
+
+  const roles = ["a Data Analyst", "a Web Developer", "a Designer", "a Freelancer"];
+  const [currentRoleIndex, setCurrentRoleIndex] = useState(0);
+
+  function switchRole() {
+    setCurrentRoleIndex((prevIndex) => (prevIndex + 1) % roles.length);
+  }
+
+  useEffect(function setupInterval() {
+    const interval = setInterval(switchRole, 2000); 
+
+    return function cleanupInterval() {
+      clearInterval(interval);
+    };
+  }, );
+  return (
+    <>
+      <section className="hero">
+        <div className="content">
+          <h1>Hi, I am Aderemi</h1>
+        </div>
+        <br />
+        <div className="content1">
+          <h1>I am {roles[currentRoleIndex]}</h1>
+        </div>
+      </section>
+      <Skills />
+    </>
+  );
+}
+
+export default Home;
