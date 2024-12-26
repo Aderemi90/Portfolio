5dd9153daa4a0ea42776003a28aaee00700243fa
Aderemi Oje <remoj2003@yahoo.com>
2024-12-27 00:21:17 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
 import Home from './Home'
 import About from './About'
 import Header from './Header'
 import Footer from './Footer'
-// import Contact from './Contact'
 import Services from './Services'
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/services" element={<Services />} />
-                {/* <Route path="/contact" element={<Contact />} /> */}
             </Routes>
             <Footer />
         </Router>
